17ddebb6e0c579dc3828e31fe85b5ec9112e2da9
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-15 16:00:45 -0600

    java exercise beginnings

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,17 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(" World");
+        int MyFavoriteNumber;
+        String myString;
+        float myNumber;
+        MyFavoriteNumber = 3;
+        myString = "this is my first string in java!";
+        myNumber = 123; // completed 3.14
+        System.out.println("");
+
+        System.out.println(MyFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+    }
 }
