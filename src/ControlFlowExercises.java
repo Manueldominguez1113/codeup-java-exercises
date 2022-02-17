ee4068d8119140bd27cb020f379f5762b0e98f6a
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-17 14:05:47 -0600

    for loop finished

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int i =5;
-//        while(i <15){
+//        while(i <=15){
 //            System.out.println(i);
 //            i++;
-
+ //======-------------- do-while loops
 //        int i =0;
 //        do {
 //            i+=2;
 //            System.out.println(i);
-//        } while (i<100);
+//        } while (i<=100);
 
 //        int i =100;
 //        do {
@@ -17,11 +17,35 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        } while (i>-10);
 
-        int i =2;
-        do {
-            i*=i;
-            System.out.println(i);
-        } while (i<60000); // wont run corrently once passes 1million. 60k works better.
+//        int i =2;
+//        do {
+//            i*=i;
+//            System.out.println(i);
+//        } while (i<60000); // wont run corrently once passes 1million. 60k works better.
+
+//        =-----------for loops
+
+//        for(int i=5;i<=15;i++) {
+//            System.out.println(i);
+//        }
+
+//        for(int i=0;i<=100;i+=2){
+//            System.out.println(i);
+//        }
+
+//        for(int i =100; i>-10;i-=5){
+//            System.out.println(i);
+//        }
+
+//        int i = 2;
+//        System.out.println(i);
+//        for(i=2;i<60000;){
+//            i*=i;
+//            System.out.println(i);
+//
+//        }
+
+
 
     }
 }
