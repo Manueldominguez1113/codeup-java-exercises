54bc55f578800b7e776b207a7f7d02fadab14c61
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-17 14:17:47 -0600

    completed fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -45,6 +45,21 @@ public class ControlFlowExercises {
 //
 //        }
 
+//------ fizzbuzz test
+    int i= 1;
+    do{
+        if(i%3==0 & i%5==0){
+            System.out.println("FizzBuzz");
+        }else if(i%3==0){
+            System.out.println("Fizz");
+        } else if(i%5==0){
+            System.out.println("Buzz");
+        } else{
+            System.out.println(i);
+        }
+        i++;
+
+    }while(i<=100);
 
 
     }
