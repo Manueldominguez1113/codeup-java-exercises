f948f9f2602a88e3b4f37a83152794d1bea8962a
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-22 16:02:55 -0600

    working on packages

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,8 +1,57 @@
 package util;
+
 import java.util.Scanner;
+
 public class Input {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static Scanner scanner;
+
+    String getString() {
+        System.out.println("please write out your string");
+        return scanner.nextLine();
+    }
+
+    boolean yesNo() // if y || yes || yeah, true, else false
+    {
+        System.out.println("yes or no?");
+        String response = scanner.next();
+        return response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yeah") || response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("True");
+    }
+   
+    int getInt(int min, int max)  // continue to prompt for input until it is within the limits of min and max.
+    {
+        int input=-120;
+        while(input<min | input>max){
+        System.out.format("\ninput your number between %s and %s\n",min, max);
+        input = scanner.nextInt();
+        if (input<min | input>max) {
+            System.out.println("invalid number.");
+        }
+        }
+        return input;
 
     }
+
+    int getInt(){
+        System.out.println("input your number:\n");
+        return scanner.nextInt();
+    }
+    double getDouble(double min, double max)
+    {
+        double input=-20.00;
+        while(input<min | input>max){
+            System.out.format("\ninput your number between %s and %s\n",min, max);
+            input = scanner.nextInt();
+            if (input<min | input>max) {
+                System.out.println("invalid number.");
+            }
+        }
+        return input;
+
+    }
+    double getDouble(){
+        System.out.println("input your number:\n");
+        return scanner.nextInt();
+    }
+
 }
+
