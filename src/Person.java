ab5c00fb4d03f5cb9721e9ed64c7dc9e31cb276e
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-22 15:30:29 -0600

    completed persons

--- src/Person.java
+++ src/Person.java
@@ -11,7 +11,26 @@
          alex.sayHello();
          alex.setName("Albert");
          alex.sayHello();
-     }
+
+         /*Person person1 = new Person("John");
+         Person person2 = new Person("John");
+         System.out.println(person1.getName().equals(person2.getName())); // john is john true
+         System.out.println(person1 == person2);                            // person1 john is not person2 john though
+*/
+         /*Person person1 = new Person("John");
+         Person person2 = person1;
+         System.out.println(person1 == person2);*/
+
+         /*Person person1 = new Person("John");
+         Person person2 = person1;
+         System.out.println(person1.getName());
+         System.out.println(person2.getName());
+         person2.setName("Jane");                   // Surprised to see that setting person 2 changes both names.
+         System.out.println(person1.getName());
+         System.out.println(person2.getName());
+         */
+
+        }
         public String getName(){
             return Person.name;
 //DONE: return the person's name
