b2dcc11297ef6fae8c9979a92d920489ecd8db52
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-24 19:10:46 -0600

    completed bonus of circle

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,15 +1,21 @@
 package shapes;
 import util.Input;
 public class CircleApp {
-
+    private static int circle;
     public static void main(String[] args) {
         System.out.println("ready to make circles?");
-        while(Input.yesNo()) {
-            System.out.println("Input the radius of your circle:");
-            new Circle(Input.getDouble());
+        while(Input.yesNo()){
+        System.out.println("Input the radius of your circle:");
+        new Circle(Input.getDouble());
+        System.out.println("want to make another?");
+        circle++;
 
-            System.out.println("want to make another?");
         }
+            System.out.println("good work! we've created "+circle+ " circles!");
+
+
 
     }
+
+
 }
