e599907b66c5993c137be7e6488c5b2e2c8508f1
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-24 18:14:40 -0600

    completed input test

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,21 +3,22 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private static Scanner scanner;
 
-    String getString() {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getString() {
         System.out.println("please write out your string");
         return scanner.nextLine();
     }
 
-    boolean yesNo() // if y || yes || yeah, true, else false
+    public static boolean yesNo() // if y || yes || yeah, true, else false
     {
         System.out.println("yes or no?");
         String response = scanner.next();
         return response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yeah") || response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("True");
     }
-   
-    int getInt(int min, int max)  // continue to prompt for input until it is within the limits of min and max.
+
+    public static int getInt(int min, int max)  // continue to prompt for input until it is within the limits of min and max.
     {
         int input=-120;
         while(input<min | input>max){
@@ -31,16 +32,16 @@ public class Input {
 
     }
 
-    int getInt(){
+    public static int getInt(){
         System.out.println("input your number:\n");
         return scanner.nextInt();
     }
-    double getDouble(double min, double max)
+    public static double getDouble(double min, double max)
     {
-        double input=-20.00;
+        double input=0.00;
         while(input<min | input>max){
             System.out.format("\ninput your number between %s and %s\n",min, max);
-            input = scanner.nextInt();
+            input = scanner.nextDouble();
             if (input<min | input>max) {
                 System.out.println("invalid number.");
             }
@@ -48,9 +49,9 @@ public class Input {
         return input;
 
     }
-    double getDouble(){
+    public static double getDouble(){
         System.out.println("input your number:\n");
-        return scanner.nextInt();
+        return scanner.nextDouble();
     }
 
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,7 +1,16 @@
 package util;
 
+
 public class InputTest {
     public static void main(String[] args) {
+        System.out.println(Input.getString());
+        System.out.println(Input.yesNo());
+        System.out.println(Input.getInt());
+        System.out.println(Input.getInt(1,10));
+        System.out.println(Input.getDouble());
+        System.out.println(Input.getDouble(0.00,10.00));
+
+
     }
 
 }
