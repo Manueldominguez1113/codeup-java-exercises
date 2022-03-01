72911221fb74cd5217cdf5228b4b20227e9088d7
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-03-01 09:26:07 -0600

    moring exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers)); // need to string to pull convert it otherwise this
+                                        // outputs the location of the array, which i cant use right now
+    }
+}
