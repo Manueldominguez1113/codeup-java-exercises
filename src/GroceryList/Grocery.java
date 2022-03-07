e8e4273c05ea60ec89ad2a46a356535f83daf874
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-03-07 14:20:32 -0600

    got functionality with grocery app. now to make it full and give it sorting and edit ability

--- src/GroceryList/Grocery.java
+++ src/GroceryList/Grocery.java
@@ -1,30 +1,125 @@
 package GroceryList;
 
+import util.Input;
+
 import java.util.ArrayList;
 
 public class Grocery {
 
     private String name;
     private String category;
-    public ArrayList<Grocery> Groceries = new ArrayList<>();
+    private int quantity;
+    static ArrayList<Grocery> groceries= new ArrayList<>();
 
     public Grocery(String item, String category) {
-this.name = item;
-this.category = category;
-Groceries.add(this);
+        this.name = item;
+        this.category = category;
+    }
+
+
+    public Grocery(Grocery item, int quantity) {
+        this.name = item.name;
+        this.category = item.category;
+        this.quantity = quantity;
 
     }
 
-    static Grocery[] groceryList = Grocerylist.List();
+    static Grocery[] groceryList = Grocerylist.List();  //full list of store products
 
-    public static void section(String category){
-        for(Grocery item: groceryList){
-            if (item.category.equalsIgnoreCase(category)|| category.equalsIgnoreCase("all")){
+
+
+    public static void section(String category) {
+        for (Grocery item : groceryList) {
+            if (item.category.equalsIgnoreCase(category) || category.equalsIgnoreCase("all")) {
                 System.out.println("----------------------------");
-                System.out.println(item.name +" , " + item.category);
+                System.out.println(item.name + " , " + item.category);
             }
         }
     }
+
+    public static Grocery selection(String input) {
+        for (Grocery item : groceryList) {
+            if (input.equalsIgnoreCase(item.name)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public static void addThis(Grocery item, int count) {
+        groceries.add(new Grocery(item, count));
+    }
+
+    public static void viewList(){
+        int i =1;
+
+        for (Grocery grocer : groceries) {
+            System.out.println(grocer.name+" " +grocer.category+" "+ grocer.quantity);
+            i++;
+        }
+    }
+
+
+
+    public static void groceryList() {
+        boolean createList = true;
+        boolean newItem = true;
+        String addItem;
+        int count;
+        while (createList) {
+
+            while (newItem) {
+
+                System.out.println("choose a category: \n1.fruits\n2.vegetables\n3.meat\n4.dairy\n5.all");
+                int category = Input.getInt(1, 5);
+                if (category == 1) {
+                    do {
+                        System.out.println("select a fruit");// link to list
+                        Grocery.section("fruit");
+                        addItem = Input.getString();
+
+                        if (Grocery.selection(addItem) == null) {
+                            System.out.println("error! not found, try again");
+                        }
+                    } while (Grocery.selection(addItem) == null);
+                    System.out.println("how many "+addItem+"s would you like?");
+                    count = Input.getInt();
+                    if (count !=0){
+                        Grocery.addThis(Grocery.selection(addItem), count);
+                    }
+                } else if (category == 2) {
+                    System.out.println("select a vegetable");//link to list
+                    Grocery.section("vegetables");
+                    addItem = Input.getString();
+
+                } else if (category == 3) {
+                    System.out.println("select a meat");//link to list
+                    Grocery.section("meat");
+                    addItem = Input.getString();
+
+                } else if (category == 4) {
+                    System.out.println("select a dairy product"); //link to list
+                    Grocery.section("dairy");
+                    addItem = Input.getString();
+
+                } else if (category == 5) {
+                    System.out.println("select from list");
+                    Grocery.section("all");
+                    addItem = Input.getString();
+
+                } else {
+                    System.out.println("sorry, we didnt get the category you were looking for.");
+                }
+
+                System.out.println("would you like to enter a new item?");
+                newItem = Input.yesNo();
+            }
+
+            System.out.println("Would you like to continue on your list? Y/N");
+            createList = Input.yesNo();
+        }
+    }
+
 }
 
 
--- src/GroceryList/MainGroceryApp.java
+++ src/GroceryList/MainGroceryApp.java
@@ -3,17 +3,16 @@ import util.Input;
 public class MainGroceryApp {
     public static void main(String[] args) {
         int choice=0;
-        Customer customer=new Customer();
 
 
         while(choice != 3) {
             System.out.println("what would you like to do?(enter the number) \n1. add to your grocery list\n 2. view your current grocery list\n3. finish your list");
             choice = Input.getInt(1, 3);
             if (choice == 1) {
-                Customer.groceryList();
+                Grocery.groceryList();
             }
             if (choice == 2) {
-                // if 2, show the customer.grocerylist + the quantities hashmap in a sout and ask if they would like to edit it. maybe find a way to motion to finialize from here?
+                Grocery.viewList();
             }
             if (choice == 3) {
                 System.out.println("here is your list..");
