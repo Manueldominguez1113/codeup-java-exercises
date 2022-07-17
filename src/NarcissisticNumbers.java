2d058779015be22a168a5bfe813fd6f03dedd284
Manuel <manuel.dominguez1113@gmail.com>
2022-07-16 22:01:53 -0500

    completed narcissistic numbers

--- src/NarcissisticNumbers.java
+++ src/NarcissisticNumbers.java
@@ -2,18 +2,38 @@
 public class NarcissisticNumbers {
 
     public static void main(String[] args) {
-        System.out.println(NNumbers(153));
+        NNumberGen();
     }
 
-    public static int NNumbers(int number){
-        String n= String.valueOf(number);
+    //this function takes in the number, and responds with whether it's narcissistic or not
+    public static boolean NNumbers(int number) {
+        String n = String.valueOf(number);
         int length = n.length();
-        n.split("");
+        String[] digits = n.split("");
+        int total = 0;
+        for (String num : digits) {
+            double squared = Math.pow(Integer.parseInt(num), length);
+            total += squared;
+            // System.out.println(num+"^"+ length+" = " + squared);
+        }
+        // System.out.println(" = "+total);
+        return total == number;
+    }
 
-        return length;
+    //need to make a new function to generate numbers and output the first 25 numbers
+    public static void NNumberGen() {
+        System.out.println("beginning to generate narcissistic numbers...");
+        for (int i = 0; i <= 9930000; i++) {
+            if (NNumbers(i)) {
+                System.out.println(i + " is a narcissistic number");
+            }
+        }
+
     }
 
 }
+
+
 /*
 prompt:         Narcissistic Numbers
 A narcissistic number is a non-negative integer (n) with m digits where the sum of all the individual digits raised to the power m is equal to n.
