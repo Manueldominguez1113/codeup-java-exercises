e1174fb6c312ff3744803da0e0cee4c364ef0b02
Manuel <manuel.dominguez1113@gmail.com>
2022-08-03 17:23:45 -0500

    1

--- src/pairingStrings.java
+++ src/pairingStrings.java
@@ -2,8 +2,8 @@ public class pairingStrings {
     public static void main(String[] args) {
         String[] answer= solution("abc");
         String sol= "abc";
-        System.out.println((double)sol.length()/2);
-        System.out.println((int)Math.ceil((sol.length()/2)+0.5));
+     //   System.out.println((double)sol.length()/2);
+      //  System.out.println((int)Math.ceil((sol.length()/2)+0.5));
     for(String pair: answer){
         System.out.println(pair);
     }
@@ -13,15 +13,17 @@ public class pairingStrings {
     public static String[] solution(String s) {
         //Write your code here
 
-        System.out.println();
+       // System.out.println();
         String[] answer=new String[(int)Math.ceil((s.length()/2)+0.5)];
         int index=0;
         for(int i=0;i<s.length();i++){
         if(i+1<s.length()){
             answer[index]= ""+s.charAt(i)+s.charAt(i+1);
             index++;
+            i++; // to skip the next one
         }
         if(i+1==s.length()){
+            System.out.println(i);
             answer[index]= s.charAt(i)+"_";
         }
 
